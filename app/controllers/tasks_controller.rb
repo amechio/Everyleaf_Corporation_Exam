95581201681ae2066809fed77d51b3872bf3ff42
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-11 14:03:00 +0900

    タスクの削除機能を作成しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -10,22 +11,19 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to task_path, notice: "タスクを作成しました！"
+      redirect_to tasks_path, notice: "タスクを作成しました！"
     else
       render :new
     end
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path, notice: "タスクを編集しました！"
     else
@@ -33,9 +31,18 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクを削除しました！"
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
