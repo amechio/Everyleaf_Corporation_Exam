efa23c7d3ad9caedda16168c16c4bb560a3a0903
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-11 13:07:22 +0900

    タスクをDBに登録できるようにしました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(params[:task])
+    redirect_to new_task_path
+  end
 end
