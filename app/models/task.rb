8288c181c2242b86a5373f321f64afb207ebb275
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-12 14:51:39 +0900

    model specでのテスト項目の実施が完了しました。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
-  validates :details, length: { maximum: 250 }
+  validates :details, presence: true, length: { maximum: 250 }
 end
