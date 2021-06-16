924142742ff063764801217f9833ec18fe5981b1
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-16 10:46:08 +0900

    usersテーブルを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_051159) do
+ActiveRecord::Schema.define(version: 2021_06_16_014322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_06_15_051159) do
     t.integer "status", default: 1, null: false
     t.integer "priority", default: 1, null: false
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
