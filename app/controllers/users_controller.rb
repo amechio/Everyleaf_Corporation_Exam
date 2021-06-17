17ce20c69e3ded559cc364dfcaf47c659fbc36a1
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-17 13:28:11 +0900

    管理者機能（ユーザー一覧表示、ユーザー作成、ユーザー編集、ユーザー削除）を実装しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  before_action :to_user_profile, only: [:new, :create]
   def new
     @user = User.new
   end
@@ -18,5 +19,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def to_user_profile
+    redirect_to tasks_path, notice: "ログインしてます" if current_user
   end
 end
