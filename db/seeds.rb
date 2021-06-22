13edda02d21387160060ce2ce69eec3d0c12f94a
Ryosuke　Hara <h.r.hrsy0824@gmail.com>
2021-06-22 15:46:39 +0900

    User、Task、Labelテーブルそれぞれ10件のレコードを作成するため、seed.rbを編集しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'test', email: 'test@test.com', password_digest: 'testdayo')
-5.times do |i|
+
+10.times do |i|
   Label.create!(name: "sample#{i + 1}")
+end
+
+10.times do |i|
+  User.create!(id: "#{i + 1}", name: "test#{i + 1}", email: "test#{i + 1}@test#{i + 1}.com", password: "test#{i + 1}#{i + 1}#{i + 1}#{i + 1}", role: 'normal')
+end
+
+10.times do |i|
+  Task.create!(title: "test#{i + 1}", details: "Test#{i + 1}", limit: "now()", status: "未着手", priority: "中", user_id: "#{i + 1}" )
 end
